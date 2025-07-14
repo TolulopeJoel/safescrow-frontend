c79066765dc0808a322ea724349c70822c49a3b9
Tolu Joel <toluisjoel@gmail.com>
2025-07-14 15:55:09 +0100

    chore: Add new external icons

--- src/components/icons/ExternalIcons.tsx
+++ src/components/icons/ExternalIcons.tsx
@@ -13,3 +13,10 @@ export const NairaIcon = (props: SVGProps<SVGSVGElement>) => (
 export const ProfileIcon = (props: SVGProps<SVGSVGElement>) => (
     <svg xmlns="http://www.w3.org/2000/svg" height={24} viewBox="0 -960 960 960" width={24} fill="currentColor" {...props}><path d="M480-440q-66 0-113-47t-47-113v-140q0-25 17.5-42.5T380-800q15 0 28.5 7t21.5 20q8-13 21.5-20t28.5-7q15 0 28.5 7t21.5 20q8-13 21.5-20t28.5-7q25 0 42.5 17.5T640-740v140q0 66-47 113t-113 47Zm0-80q33 0 56.5-23.5T560-600v-100H400v100q0 33 23.5 56.5T480-520ZM160-120v-112q0-34 17.5-62.5T224-338q62-31 126-46.5T480-400q66 0 130 15.5T736-338q29 15 46.5 43.5T800-232v112H160Zm80-80h480v-32q0-11-5.5-20T700-266q-54-27-109-40.5T480-320q-56 0-111 13.5T260-266q-9 5-14.5 14t-5.5 20v32Zm240 0Zm0-500Z" /></svg>
 )
+
+export const CalendarIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" /></svg>
+);
+export const TruckIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 17V5a1 1 0 011-1h5a1 1 0 011 1v12m-7 0a2 2 0 11-4 0m4 0a2 2 0 104 0m-4 0h4m4 0a2 2 0 104 0m-4 0a2 2 0 11-4 0m4 0V9a1 1 0 00-1-1h-3V5a1 1 0 00-1-1H9" /></svg>
+);
