fbbf270105a17620e918c0c8648677e371d5e15e
Tolu Joel <toluisjoel@gmail.com>
2025-07-21 06:18:06 +0100

    feat (orders): Add sidebar and top bar to order creation pages

--- src/pages/CreateOrderPage.tsx
+++ src/pages/CreateOrderPage.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { CreateOrderWizard } from 'components/orders';
+import { Topbar, SidebarNav } from 'components/layout';
 
 const CreateOrderPage: React.FC = () => {
-  return <CreateOrderWizard />;
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Topbar />
+      <div className="flex">
+        <SidebarNav />
+        <main className="flex-1 p-4 sm:p-8 pt-0">
+          <CreateOrderWizard />
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default CreateOrderPage; 
