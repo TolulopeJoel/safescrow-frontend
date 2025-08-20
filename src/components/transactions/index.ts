12f52d27f98e7878293b3e7e253698db31f9f489
Tolu Joel <toluisjoel@gmail.com>
2025-08-20 06:43:23 +0100

    refactor: Remove `TransactionsTable` component

--- src/components/transactions/index.ts
+++ src/components/transactions/index.ts
@@ -1,4 +1,3 @@
 export { default as StatsOverview } from './StatsOverview';
-export { default as TransactionsTable } from './TransactionsTable'
 export { default as WalletTransactionCard } from './WalletTransactionCard'
 export { default as TransactionStatusBadge } from './TransactionStatusBadge'
